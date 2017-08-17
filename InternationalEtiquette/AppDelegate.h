a460d8c8dccc3f5a3b9cdf0cce7b07f667ef4611
Young-San Roy <584487926@qq.com>
2017-08-17 18:37:56 +0800

    Initial Commit

---
+++ InternationalEtiquette/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  InternationalEtiquette
+//
+//  Created by 孤岛 on 2017/8/17.
+//  Copyright © 2017年 孤岛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
